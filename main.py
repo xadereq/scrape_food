831f62af4c21acd2ed3151460d8bb2645b0aff50
Piotr Piskiewicz <piotr.piskiewicz@altalog.pl>
2019-01-10 02:10:05 +0100

    add food

--- main.py
+++ main.py
@@ -8,26 +8,60 @@ from pathlib import Path
 BASE_URL = "https://www.maczfit.pl/menu/"
 
 
-class Menu:
+class Maczfit:
     def __init__(self):
         self.base_url = BASE_URL
         self.content = self.get_content()
+        self.menu = self.get_menu()
+        self.days = {}
+        self.dinners = []
+        self.get_days()
+        self.get_dinners()
+        self.print_dinners()
 
-    def get_content(self, diet_type="fit"):
+    @staticmethod
+    def get_content(diet_type="fit"):
         full_url = urljoin(BASE_URL, diet_type)
         data = requests.get(full_url)
         return data.content
 
-    def get_menu(self):
+    @staticmethod
+    def drop_whitespaces(string):
+        while '  ' in string:
+            string = string.replace('  ', ' ')
+        return string.strip()
+
+    @staticmethod
+    def get_menu():
         # soup = BeautifulSoup(get_content(), 'html.parser')
-        soup = BeautifulSoup(Path("/tmp/test.html").read_text(), 'html.parser')
+        soup = BeautifulSoup(Path("test.html").read_text(), 'html.parser')
         table = soup.select("#content > div.section.woocommerce.menu-pane > div > div > div.menu > table > tbody")
         menu = table[0].find_all(recursive=False)
-        for days in menu:
-            for day in days:
-                print(day.string)
-        return print("XD")
+        return menu
+
+    def get_days(self):
+        for index, item in enumerate(self.menu, start=1):
+            day = item.find_all("td", class_="label", limit=1)
+            for date in day:
+                date = self.drop_whitespaces(date.text)
+                self.days[index] = date
+        pass
+
+    def get_dinners(self):
+        for index, item in enumerate(self.menu, start=1):
+            dinners = item.select("tbody > tr")
+            dinners.pop()
+            for dinner in dinners:
+                dinner = dinner.find_all("td", class_="value")
+                for food in dinner:
+                    food = self.drop_whitespaces(food.text)
+                    print(index)
+                    print(food)
+                    print(type(food))
+            print("NEXT DAY ==================")
+
+    def print_dinners(self):
+        return print(self.dinners)
 
 
-test = Menu()
-test.get_menu()
+test = Maczfit()
